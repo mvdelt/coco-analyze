a3f23a98f3ec3cb67e2915f22e954172647b3f29
mvdelt <mvdelt@gmail.com>
2020-06-18 01:49:49 +0900

    j) update

--- pycocotools/mask_pyx_j.py
+++ pycocotools/mask_pyx_j.py
@@ -1,8 +1,10 @@
 from setuptools import setup
 from Cython.Build import cythonize
+import numpy
 
 setup(
     name='mask_pyx_j',
-    ext_modules=cythonize(["_mask.pyx"]),
+    ext_modules=cythonize(["/content/cocoAnalyze/pycocotools/_mask.pyx"]),
+    include_dirs=[numpy.get_include()],
     zip_safe=False,
 )
